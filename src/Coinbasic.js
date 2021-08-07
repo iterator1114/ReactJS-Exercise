ffa4429ae26616c1b26c38ba46914c1ee45dce69
Abhishek <coder.shrivas@gmail.com>
2021-08-08 00:52:55 +0530

    Init

---
+++ src/Coinbasic.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Counter from './Counter';
+
+class Coinbasic extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Let's the number flip</h1>
+                <Counter />
+            </div>
+        )
+    }
+}
+
+export default Coinbasic;
